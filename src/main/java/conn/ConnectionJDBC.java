8a5b5a81aad4894933618b90e2ce98a4881394aa
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-07-16 15:44:33 -0300

    conected to remote mysql successfully

--- src/main/java/conn/ConnectionJDBC.java
+++ src/main/java/conn/ConnectionJDBC.java
@@ -6,18 +6,25 @@ import java.sql.SQLException;
 
 public class ConnectionJDBC {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Password pwConnection = new Password();
+        Connection conn = null;
         String pw = pwConnection.passwd;
         String url = pwConnection.urlConn;
         String user = pwConnection.user;
 
         System.out.println("senha: " + pw + "\nUsuario: " + user + "\nUrl: " + url);
+
         try {
-            Connection conn = DriverManager.getConnection(url, user, pw);
+            conn = DriverManager.getConnection(url, user, pw);
+            System.out.println("Conectado ao MYSQL com sucesso.");
         } catch (Exception e) {
             System.out.println("Erro");
+        } finally {
+            conn.close();
         }
+
+        System.out.println(conn);
     }
 }
