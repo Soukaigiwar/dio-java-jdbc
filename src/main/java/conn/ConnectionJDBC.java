96b1535571add824a4b56350716004a9eede3164
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-07-16 15:21:35 -0300

    gitignore added

--- src/main/java/conn/ConnectionJDBC.java
+++ src/main/java/conn/ConnectionJDBC.java
@@ -6,8 +6,18 @@ import java.sql.SQLException;
 
 public class ConnectionJDBC {
 
-    public static void main(String[] args) throws SQLException {
-        String urlConnection = "jdbc:mysql:http://192.185.217.159";
-        Connection conn = DriverManager.getConnection(urlConnection,"fastf981_dio","");
+    public static void main(String[] args) {
+
+        Password pwConnection = new Password();
+        String pw = pwConnection.passwd;
+        String url = pwConnection.urlConn;
+        String user = pwConnection.user;
+
+        System.out.println("senha: " + pw + "\nUsuario: " + user + "\nUrl: " + url);
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pw);
+        } catch (Exception e) {
+            System.out.println("Erro");
+        }
     }
 }
